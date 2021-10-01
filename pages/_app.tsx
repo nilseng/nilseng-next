222b8678dd56e3f795e1a0bf589b9ba79f00b3ec
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2021-10-01 22:59:17 +0200

    adding google analytics tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,31 @@
-import "../styles/globals.scss";
-import "mapbox-gl/dist/mapbox-gl.css";
 import type { AppProps } from "next/app";
+
+import * as ga from "../lib/ga";
 import { Navbar } from "../components/Navbar";
 
+import "../styles/globals.scss";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 function NilsengApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Navbar />
