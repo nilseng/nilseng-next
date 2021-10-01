2290ea362847a0164fa420bc91aa71b8afd8a6d5
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2021-10-02 00:05:03 +0200

    closing mobile nav menu on click

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -67,7 +67,10 @@ export const Navbar = () => {
           isMenuOpen ? "" : "hidden"
         }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
       >
-        <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+        <div
+          className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto"
+          onClick={() => setIsMenuOpen(false)}
+        >
           <Link href="/blog">
             <a className="text-sm lg:inline-flex lg:w-auto w-full px-6 py-2 rounded text-gray-50 font-light items-center justify-center hover:text-white">
               Blog
