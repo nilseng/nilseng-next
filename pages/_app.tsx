57ba3b308e07a1706ae8ac1c35bcb5186ea8196d
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-01-23 12:29:04 +0100

    nit: moves ga function out of component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,21 @@
 import type { AppProps } from "next/app";
 
-import * as ga from "../lib/ga";
 import { Navbar } from "../components/Navbar";
+import * as ga from "../lib/ga";
 
-import "../styles/globals.scss";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import "../styles/globals.scss";
+
+const handleRouteChange = (url: string) => {
+  ga.pageview(url);
+};
 
 function NilsengApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      ga.pageview(url);
-    };
     //When the component is mounted, subscribe to router changes
     //and log those page views
     router.events.on("routeChangeComplete", handleRouteChange);
