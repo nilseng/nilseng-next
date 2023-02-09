f458b4203f86ec9d3d9662f93f543af030b59f8f
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-09 19:38:08 +0100

    chore: removes useless comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,12 +16,7 @@ function NilsengApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
     router.events.on("routeChangeComplete", handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
